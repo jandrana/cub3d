e0e2dbb20f053b77902db114b1c7f2eb17043686
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-02-11 19:00:27 +0100

    [cub3d.h code style] Fixed norminette errors

--- include/cub3d.h
+++ include/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 11:53:03 by ana-cast          #+#    #+#             */
-/*   Updated: 2025/02/07 19:26:29 by ana-cast         ###   ########.fr       */
+/*   Updated: 2025/02/11 18:55:12 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,54 +37,54 @@
 
 // ------------------- STRUCTURES ------------------- //
 
-typedef enum    e_map_tile
+typedef enum e_map_tile
 {
-    EMPTY = 0,
+	EMPTY = 0,
 	WALL = 1,
-    PLAYER_NORTH = 'N',
-    PLAYER_SOUTH = 'S',
-    PLAYER_EAST = 'E',
-    PLAYER_WEST = 'W'
-}	            t_map_tile;
+	PLAYER_NORTH = 'N',
+	PLAYER_SOUTH = 'S',
+	PLAYER_EAST = 'E',
+	PLAYER_WEST = 'W'
+}	t_map_tile;
 
 typedef struct s_player_position
 {
-	unsigned int    row;
-	unsigned int    col;
+	unsigned int	row;
+	unsigned int	col;
 }				t_player_position;
 
 typedef struct s_map
 {
 	unsigned int		rows;
 	unsigned int		cols;
-    t_player_position   player;
-	t_map_tile		    **mt;
+	t_player_position	player;
+	t_map_tile			**mt;
 }				t_map;
 
 typedef struct s_color
 {
-    unsigned char r;
-    unsigned char g;
-    unsigned char b;
-}               t_color;
+	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
+}	t_color;
 
 typedef struct s_graphics
 {
-    mlx_t           *mlx;
-    mlx_image_t     *img;
-    t_color         floor_color;
-    t_color         ceiling_color;
-    mlx_texture_t   *north_texture;
-    mlx_texture_t   *south_texture;
-    mlx_texture_t   *east_texture;
-    mlx_texture_t   *west_texture;
-}               t_graphics;
+	mlx_t			*mlx;
+	mlx_image_t		*img;
+	t_color			floor_color;
+	t_color			ceiling_color;
+	mlx_texture_t	*north_texture;
+	mlx_texture_t	*south_texture;
+	mlx_texture_t	*east_texture;
+	mlx_texture_t	*west_texture;
+}	t_graphics;
 
 typedef struct s_game
 {
-    t_map           *map;
-    t_graphics      *graphics;
-}               t_game;
+	t_map		*map;
+	t_graphics	*graphics;
+}	t_game;
 
 
 // ------------------------------------------------------ //
