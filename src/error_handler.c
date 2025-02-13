4b2ac64b0a4786f2792023a137c96da7b4d3709e
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-02-13 20:54:42 +0100

    [Error Handler] Implemented basic working version

---
+++ src/error_handler.c
@@ -0,0 +1,71 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error_handler.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 18:42:29 by ana-cast          #+#    #+#             */
+/*   Updated: 2025/02/13 20:53:37 by ana-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cub3d.h>
+#include <stdio.h>
+#include <unistd.h>
+
+static void	end_mlx(t_graphics *graphics)
+{
+	int	i;
+
+	i = -1;
+	if (!graphics)
+		return ;
+	while (++i < 4)
+	{
+		if (graphics->textures[i])
+			mlx_delete_texture(graphics->textures[i]);
+	}
+	if (graphics->img)
+		mlx_delete_image(graphics->mlx, graphics->img);
+	if (graphics->mlx)
+		mlx_terminate(graphics->mlx);
+	free(graphics);
+}
+
+static void	free_map(t_map	*map)
+{
+	int	i;
+
+	i = -1;
+	if (!map)
+		return ;
+	while (map->mt && ++i < (int)map->rows)
+		free(map->mt[i]);
+	free(map->mt);
+	free(map);
+}
+
+void	free_game(t_game *game)
+{
+	if (!game)
+		return ;
+	if (game->map)
+		free_map(game->map);
+	if (game->graphics)
+		end_mlx(game->graphics);
+}
+
+void	error_exit(t_game *game, const char *msg, ...)
+{
+	va_list	args;
+
+	ft_putstr_fd(RED BOLD "Error\n  " WHITE, STDERR_FILENO);
+	va_start(args, msg);
+	vfprintf(stderr, msg, args);
+	va_end(args);
+	ft_putstr_fd("\n", STDERR_FILENO);
+	if (game)
+		free_game(game);
+	exit(EXIT_FAILURE);
+}
