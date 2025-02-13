56a538e90f522b4f589f71247097b19c833d9d78
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-02-13 20:56:33 +0100

    [main.c] Changes to main implementing parser

--- src/main.c
+++ src/main.c
@@ -6,14 +6,27 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 16:27:29 by jorvarea          #+#    #+#             */
-/*   Updated: 2025/02/07 19:20:09 by ana-cast         ###   ########.fr       */
+/*   Updated: 2025/02/13 20:55:58 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
-int main(void)
+t_game	*init_game(void)
 {
-    printf("Hello world!\n");
-    return (0);   
+	t_game	*game;
+
+	game = (t_game *)malloc(sizeof(t_game));
+	game->map = NULL;
+	game->graphics = NULL;
+	return (game);
+}
+
+int	main(int argc, char **argv)
+{
+	t_game	*game;
+
+	game = init_game();
+	game->map = parser(argc, argv);
+	return (0);
 }
