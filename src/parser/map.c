a17a4822b8aa19e37f4caa69d896eb50216e7769
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-02-24 21:27:03 +0100

    [map.c] check_map_line implementation

--- src/parser/map.c
+++ src/parser/map.c
@@ -6,8 +6,22 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:56:15 by ana-cast          #+#    #+#             */
-/*   Updated: 2025/02/18 17:56:21 by ana-cast         ###   ########.fr       */
+/*   Updated: 2025/02/24 21:26:54 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
+
+t_line_type	check_map_line(t_game *game, char *line, size_t row)
+{
+	int	i;
+
+	i = -1;
+	while (line[++i])
+	{
+		if (!ft_strchr(VALID_MAP_CHARS, line[i]))
+			error_exit(game, E_MAP_CHAR, line[i], row, i);
+	}
+	return (MAP_LINE);
+}
+
