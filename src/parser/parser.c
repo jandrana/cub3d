3925140d0302392e26cc44086d35010e3ab08c21
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-02-24 21:21:06 +0100

    [parser: map] Initial map parsing implementation

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -6,24 +6,52 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 19:57:43 by ana-cast          #+#    #+#             */
-/*   Updated: 2025/02/18 19:12:24 by ana-cast         ###   ########.fr       */
+/*   Updated: 2025/02/24 21:20:16 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <cub3d.h>
 
+void	udpate_map_sizes(t_game *game, char *filename)
+{
+	int		fd;
+	char	*line;
+	size_t	len;
+
+	fd = open_map_file(game, filename);
+	line = get_next_line(fd);
+	while (line)
+	{
+		if (get_line_type(line) == MAP_LINE)
+		{
+			len = ft_strlen(line);
+			if (len > 0 && line[len - 1] == '\n')
+				len--;
+			if (len > game->map->cols)
+				game->map->cols = len;
+			game->map->rows++;
+		}
+		free_str(&line);
+		line = get_next_line(fd);
+	}
+	if (game->map->rows == 0)
+		error_exit(game, E_MAP_EMPTY);
+	close(fd);
+}
+
 t_map	*parser(t_game *game, int argc, char **argv)
 {
-	t_map	*map;
 	int		fd;
-	//char 	*first_map_line;
+	char 	*first_map_line;
 
-	map = (t_map *)malloc(sizeof(t_map));
 	if (argc != 2)
 		error_exit(game, E_ARGS_COUNT, argv[0]);
+	udpate_map_sizes(game, argv[1]);
 	fd = open_map_file(game, argv[1]);
-	parse_elements(game, fd);
-	//first_map_line = parse_elements(game, fd);
-	//parse_map(game, fd, first_map_line);
-	return (map);
+	first_map_line = parse_elements(game, fd);
+	if (!first_map_line)
+		error_exit(game, E_MAP_EMPTY);
+	//parser_map(game, fd, first_map_line);
+	close(fd);
+	return (game->map);
 }
