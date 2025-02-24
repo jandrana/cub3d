9b57a4f812e8cbd2ba1a8614c2c97dfd1b325085
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-02-24 21:27:40 +0100

    [map.c] allocate_map_tiles implementation

--- src/parser/map.c
+++ src/parser/map.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:56:15 by ana-cast          #+#    #+#             */
-/*   Updated: 2025/02/24 21:26:54 by ana-cast         ###   ########.fr       */
+/*   Updated: 2025/02/24 21:27:35 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,3 +25,19 @@ t_line_type	check_map_line(t_game *game, char *line, size_t row)
 	return (MAP_LINE);
 }
 
+void	allocate_map_tiles(t_game *game)
+{
+	size_t	i;
+
+	game->map->mt = malloc(sizeof(t_map_tile *) * game->map->rows);
+	if (!game->map->mt)
+		error_exit(game, E_MEM_ALLOC, "map rows");
+	i = 0;
+	while (i < game->map->rows)
+	{
+		game->map->mt[i] = malloc(sizeof(t_map_tile) * game->map->cols);
+		if (!game->map->mt[i])
+			error_exit(game, E_MEM_ALLOC, "map columns");
+		i++;
+	}
+}
