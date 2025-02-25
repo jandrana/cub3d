e709cf1a8e73cc8fdc54f1b36bdce0bf9f8ec6a5
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-02-25 17:10:06 +0100

    [validate_map] Implemented diagonal validation

--- src/parser/validate_map.c
+++ src/parser/validate_map.c
@@ -6,7 +6,7 @@
 /*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/24 18:49:48 by ana-cast          #+#    #+#             */
-/*   Updated: 2025/02/24 21:31:25 by ana-cast         ###   ########.fr       */
+/*   Updated: 2025/02/25 17:08:33 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,44 +16,56 @@
 
 bool	valid_tile(size_t tile, size_t max_tile)
 {
-	if (tile > 0 && tile <= max_tile)
-		return (1);
-	return (0);
+	return (tile < max_tile);
 }
 
-void	validate_walls(t_game *game, size_t row, size_t col)
+void	validate_walls(t_game *game, size_t row, size_t col, t_map_tile current)
 {
-	if (row == 0 || row == game->map->rows 
-		|| col == 0 || col == game->map->cols)
-		error_exit(game, E_MAP_WALLS);
+	bool	is_edge;
+
+	is_edge = false;
+	if (row == 0 || row > game->map->rows 
+		|| col == 0 || col > game->map->cols)
+		is_edge = true;
+	if (is_edge && current != SPACE)
+		error_exit(game, E_MAP_WALLS, row, col);
 }
 
-// check if game->map->rows/cols es + 1 o no.
+void	validate_tile(t_game *game, size_t row, size_t col, char *surrounding)
+{
+	t_map *map;
+
+	map = game->map;
+	if (valid_tile(row, map->rows) && valid_tile(col, map->cols))
+	{
+		if (!ft_strchr(surrounding, map->mt[row][col]))
+			error_exit(game, E_MAP_CHAR, map->mt[row][col], row, col);
+	}
+}
+
 void	validate_near_tiles(t_game *game, size_t row, size_t col)
 {
 	t_map_tile	current;
 	char		*surrounding;
-	
-	current = game->map->mt[row][col];
+	t_map		*map;
+
+	map = game->map;
+	current = map->mt[row][col];
 	if (current != WALL)
 	{
-		validate_walls(game, row, col);
+		validate_walls(game, row, col, current);
 		if (current == SPACE)
 			surrounding = NEAR_SPACE;
 		else
 			surrounding = NEAR_PLAYABLE;
-		if (valid_tile(row - 1, game->map->rows) 
-			&& !ft_strchr(surrounding, game->map->mt[row - 1][col]))
-			error_exit(game, E_MAP_CHAR, game->map->mt[row - 1][col], row, col);
-		if (valid_tile(row + 1, game->map->rows) 
-			&& !ft_strchr(surrounding, game->map->mt[row + 1][col]))
-			error_exit(game, E_MAP_CHAR, game->map->mt[row - 1][col], row, col);
-		if (valid_tile(col - 1, game->map->cols)
-			&& !ft_strchr(surrounding, game->map->mt[row][col - 1]))
-			error_exit(game, E_MAP_CHAR, game->map->mt[row - 1][col], row, col);
-		if (valid_tile(col + 1, game->map->cols)
-			&& !ft_strchr(surrounding, game->map->mt[row][col + 1]))
-			error_exit(game, E_MAP_CHAR, game->map->mt[row - 1][col], row, col);
+		validate_tile(game, row - 1, col, surrounding);
+		validate_tile(game, row + 1, col, surrounding);
+		validate_tile(game, row, col - 1, surrounding);
+		validate_tile(game, row, col + 1, surrounding);
+		validate_tile(game, row - 1, col - 1, surrounding);
+		validate_tile(game, row - 1, col + 1, surrounding);
+		validate_tile(game, row + 1, col - 1, surrounding);
+		validate_tile(game, row + 1, col + 1, surrounding);
 	}
 }
 
@@ -63,15 +75,15 @@ void	validate_map(t_game *game)
 	size_t	col;
 
 	row = 0;
-	while (row < game->map->rows)
+	while (row <= game->map->rows)
 	{
 		col = 0;
-		while (col < game->map->cols)
+		while (col <= game->map->cols)
 		{
 			validate_near_tiles(game, row, col);
 			col++;
 		}
-		row ++;
+		row++;
 	}
 	if (game->map->player.x < 0 || game->map->player.y < 0)
 		error_exit(game, E_MAP_NO_PLAYER);
