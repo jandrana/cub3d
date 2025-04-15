585c1733593b9b5858975322f73f072cddd1146a
jorvarea <jorvarea@users.noreply.github.com>
2025-04-15 18:01:47 +0200

    fix manage resize

--- src/manage_resize.c
+++ src/manage_resize.c
@@ -6,7 +6,7 @@
 /*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/02 16:44:23 by jorvarea          #+#    #+#             */
-/*   Updated: 2025/03/09 19:55:24 by jorvarea         ###   ########.fr       */
+/*   Updated: 2025/04/15 17:59:59 by jorvarea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,11 @@ void	manage_resize(int32_t width, int32_t height, void *ptr)
 	t_game	*game;
 
 	game = (t_game *)ptr;
+	mlx_delete_image(game->graphics->mlx, game->graphics->minimap);
+	mlx_delete_image(game->graphics->mlx, game->graphics->fps);
+	mlx_delete_image(game->graphics->mlx, game->graphics->img);
+	game->graphics->minimap = NULL;
+	game->graphics->fps = NULL;
+	game->graphics->img = NULL;
 	render_scene(game, width, height);
 }
