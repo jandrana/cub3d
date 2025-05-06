aa32686ca66adb44f8bc12f00e798a21ab8073a0
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-05-06 20:09:25 +0200

    Implemented items in map parsing

--- src/parser/map.c
+++ src/parser/map.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map.c                                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jorvarea <jorvarea@student.42malaga.com    +#+  +:+       +#+        */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 17:56:15 by ana-cast          #+#    #+#             */
-/*   Updated: 2025/03/01 18:18:37 by jorvarea         ###   ########.fr       */
+/*   Updated: 2025/05/05 17:40:21 by ana-cast         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,6 +80,87 @@ void	fill_map_line(t_game *game, char *t_line, size_t row)
 	}
 }
 
+bool	reach_player(t_game *game, size_t x, size_t y)
+{
+	char		player_char;
+	char		position_char;
+
+	player_char = game->map->mt[(int)game->player.y][(int)game->player.x];
+	position_char = game->map->mt[y][x];
+	if (x < 0 || y < 0 || x > game->map->cols || y > game->map->rows)
+		return (0);
+	if (position_char == WALL || position_char == SPACE ||
+		position_char == 'V' || position_char == 'C')
+		return (0);
+	if (position_char == player_char)
+		return (1);
+	if (position_char == EMPTY)
+		game->map->mt[y][x] = 'V';
+	else if (position_char == EMPTY)
+		game->map->mt[y][x] = 'C';
+	if (reach_player(game, x - 1, y) || reach_player(game, x + 1, y)
+		|| reach_player(game, x, y - 1)
+		|| reach_player(game, x, y + 1))
+		return (1);
+	return (0);
+}
+
+void	restore_map(t_map *map)
+{
+	unsigned int	i;
+	unsigned int	j;
+
+	i = 0;
+	while (i < map->rows)
+	{
+		j = 0;
+		while (j < map->cols)
+		{
+			if (map->mt[i][j] == 'V')
+				map->mt[i][j] = '0';
+			if (map->mt[i][j] == 'C')
+				map->mt[i][j] = 'I';
+			j++;
+		}
+		i++;
+	}
+}
+
+void	new_item(t_game *game, size_t y, size_t x, size_t item_no)
+{
+	game->map->items[item_no].x = x;
+	game->map->items[item_no].y = y;
+	game->map->items[item_no].collected = false;
+	game->map->items[item_no].sprite_frame = 0;
+	if (!reach_player(game, x, y))
+		error_exit(game, E_ITEM_REACH, y, x);
+	restore_map(game->map);
+}
+
+void	update_map_items(t_game *game)
+{
+	size_t	row;
+	size_t col;
+	size_t i;
+
+	i = 0;
+	game->map->items = (t_item *)malloc(sizeof(t_item) * game->map->n_items);
+	if (!game->map->items)
+		error_exit(game, E_MEM_ALLOC, "map items");
+	row = 0;
+	while (row <= game->map->rows)
+	{
+		col = 0;
+		while (col <= game->map->cols)
+		{
+			if (game->map->mt[row][col] == ITEM)
+				new_item(game, row, col, i++);
+			col++;
+		}
+		row++;
+	}
+}
+
 void	parser_map(t_game *game, int fd, char *map_line)
 {
 	char	*line;
@@ -99,4 +180,5 @@ void	parser_map(t_game *game, int fd, char *map_line)
 		line = get_next_line(fd);
 	}
 	validate_map(game);
+	update_map_items(game);
 }
