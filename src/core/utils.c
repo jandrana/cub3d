3ad3d093a80d3e7552d8f9a983ba5db298d1f0fc
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-06-05 14:30:56 +0200

    [🔧] New utils.c file for core/

---
+++ src/core/utils.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/27 18:02:43 by ana-cast          #+#    #+#             */
+/*   Updated: 2025/05/29 16:31:42 by ana-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "error.h"
+#include "libft.h"
+#include "MLX42/MLX42.h"
+#include "types.h"
+
+void	*safe_calloc(size_t count, size_t size, void *free_exit, char *type)
+{
+	void	*ptr;
+
+	ptr = ft_calloc(count, size);
+	if (!ptr)
+		error_exit(free_exit, E_MEM_ALLOC, type);
+	return (ptr);
+}
+
+void	safe_free(void *ptr)
+{
+	if (ptr)
+		free(ptr);
+	ptr = NULL;
+}
+
+void	delete_texture(void *texture)
+{
+	mlx_delete_texture((mlx_texture_t *)texture);
+}
+
+void	free_hlist(t_hlist **lst, void (*del)(void *))
+{
+	t_hlist	*node;
+
+	node = *lst;
+	while (node)
+	{
+		*lst = node->next;
+		(*del)(node->content);
+		free(node);
+		node = *lst;
+	}
+}
