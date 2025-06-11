9dfc47dae3881510edea17bdb232c68a25085c8d
Ana Alejandra Castillejo <yo@anaalejandra.com>
2025-06-11 19:17:11 +0200

    [⏰] Temporary header file

---
+++ include/core/cub3d.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ana-cast <ana-cast@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 11:53:03 by ana-cast          #+#    #+#             */
+/*   Updated: 2025/06/11 15:56:31 by ana-cast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
+
+// ------------------- CORE INCLUDES ------------------- //
+
+# ifndef IS_BONUS
+#  include "../mandatory/cub3d_mandatory.h"
+# else
+#  include "../bonus/cub3d_bonus.h"
+# endif
+
+// ------------------- MISSING ORGANIZATION ------------------- //
+
+#endif /* CUB3D_H */
